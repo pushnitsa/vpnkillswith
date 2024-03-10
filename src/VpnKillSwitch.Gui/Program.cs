e7a7a74acd5fd0bf6d99014bf3e1467cd2a5bb56
Dmitry Pushnitsa <d.pushnitsa@gmail.com>
2024-03-10 20:29:35 +0100

    Add DI support

--- src/VpnKillSwitch.Gui/Program.cs
+++ src/VpnKillSwitch.Gui/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
 namespace VpnKillSwitch.Gui;
 
 internal static class Program
@@ -8,9 +11,21 @@ internal static class Program
     [STAThread]
     static void Main()
     {
+        var host = Host
+            .CreateDefaultBuilder()
+            .ConfigureServices(ConfigureServices)
+            .Build();
+
+        var services = host.Services;
+
         // To customize application configuration such as set high DPI settings or default font,
         // see https://aka.ms/applicationconfiguration.
         ApplicationConfiguration.Initialize();
-        Application.Run(new Form1());
+        Application.Run(services.GetRequiredService<Form1>());
+    }
+
+    static void ConfigureServices(IServiceCollection services)
+    {
+        services.AddTransient<Form1>();
     }
 }
